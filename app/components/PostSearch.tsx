6b0a98a348d7c766c94f5f88c02cdcefccb0cc3c
Violetta Fedoseenko <viol.fedoseenko@yandex.ru>
2024-02-24 19:35:02 +0100

    client search via swr

--- app/components/PostSearch.tsx
+++ app/components/PostSearch.tsx
@@ -1,16 +1,20 @@
 'use client'
 
 import { FormEventHandler, useState } from "react"
-import { usePosts } from "../store"
+// import { usePosts } from "../store"
+import useSWR from "swr"
+import { getPostsBySearch } from "../services/getPosts"
 
 
 const PostSearch = () => {
     const [search, setSearch] = useState('')
-    const getPostsBySearch = usePosts(state => state.getPostsBySearch)
+    const {mutate} = useSWR('posts',) 
+    // const getPostsBySearch = usePosts(state => state.getPostsBySearch)
 
     const handleSubmit:FormEventHandler<HTMLFormElement> = async (e) => {
         e.preventDefault()
-        await getPostsBySearch(search)
+        const posts = await getPostsBySearch(search)
+        mutate(posts)
     }
     
     return(
--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,23 +1,27 @@
 "use client"
 
 import Link from "next/link"
-import { usePosts } from "../store"
-import { shallow } from "zustand/shallow"
-import { useEffect } from "react"
+import useSWR from "swr"
+import { getAllPosts } from "../services/getPosts"
+// import { usePosts } from "../store"
+// import { shallow } from "zustand/shallow"
+// import { useEffect } from "react"
 
 const Posts = () => {
-    const [posts, loading, getAllPosts] = usePosts((state) => [
-        state.posts,
-        state.loading,
-        state.getAllPosts,
-    ], shallow) //чтобы не было лишнего ререндера
+    const {data: posts, isLoading} = useSWR('posts', getAllPosts) //swr сам заботится когда вызывается это функция и какие будут статусы загрузки
 
-    useEffect(() => {
-        getAllPosts()
-    }, [getAllPosts])
+    // const [posts, loading, getAllPosts] = usePosts((state) => [
+    //     state.posts,
+    //     state.loading,
+    //     state.getAllPosts,
+    // ], shallow) //чтобы не было лишнего ререндера
+
+    // useEffect(() => {
+    //     getAllPosts()
+    // }, [getAllPosts])
 
     return(
-        loading 
+        isLoading 
             ? (<h3>Loading...</h3>)
             : (<ul>
                 {posts.map((post: any) => (
